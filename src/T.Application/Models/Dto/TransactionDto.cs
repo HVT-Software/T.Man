2a5f9f223538a66646e409de5ca01da68ee554cb
Hồ Văn Toàn <it.vantoan@gmail.com>
2025-05-20 17:53:01 +0700

    fix: update CategoryId to nullable in transaction models

--- src/T.Application/Models/Dto/TransactionDto.cs
+++ src/T.Application/Models/Dto/TransactionDto.cs
@@ -9,7 +9,7 @@ namespace T.Application.Models.Dto;
 public class TransactionDto {
     public Guid             Id          { get; set; }
     public Guid             UserId      { get; set; }
-    public Guid             CategoryId  { get; set; }
+    public Guid?            CategoryId  { get; set; }
     public ETransactionType Type        { get; set; }
     public decimal          Amount      { get; set; }
     public string           Description { get; set; } = string.Empty;
